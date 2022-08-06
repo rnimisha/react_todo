2c237fc57fd60cc1c8dadcc16d9727513db4f2ee
rnimisha <nimisaraut.gmail.com>
2022-08-06 16:42:55 +0545

    icon css added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,8 +4,10 @@ import './style.css';
 const Todo = () => {
   return (
     <>
-    <div class ='todo-container'>
-
+    <div className ='todo-container'>
+        <div className="todo-top-icon-container">
+            <img className="todo-icon" src="images/todo-cute-icon.png" alt="" />
+        </div>
     </div>
     </>
   )
